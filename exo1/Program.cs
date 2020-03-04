30883d5e818ef54cb3dca5d2ad2e26c4200ef0d5
Prince@ <61355057+Prince-Isasak@users.noreply.github.com>
2020-03-04 00:05:00 +0000

    my second commit

---
+++ exo1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> liste = new List<int>();
+            var som = 0;
+            while (true)
+            {
+                
+                Console.WriteLine("Enter des prix sans fcfa");
+                var user = Console.ReadLine();               
+                if (user == "c'est finis")               
+                break;
+                else
+                {
+                    var NombreUser = Convert.ToInt32(user);
+                    liste.Add(NombreUser);
+                }
+                
+            }
+            foreach (var item in liste)
+            {
+                som += item;
+            }
+            Console.WriteLine("la somme est :"+" "+ som);
+        }
+    }
+}
